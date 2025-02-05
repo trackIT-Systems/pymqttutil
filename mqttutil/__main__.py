2acab61d16a10d133b6e14433257d58b5f1a73f5
Jonas Höchst <git@jonashoechst.de>
2025-02-05 15:19:08 +0100

    implemened jsonl export

--- mqttutil/__main__.py
+++ mqttutil/__main__.py
@@ -1,11 +1,12 @@
 import argparse
 import configparser
+import datetime
 import json
 import logging
+import os
 import platform
 import time
 from ast import literal_eval
-from typing import List
 
 import paho.mqtt.client as mqtt
 import schedule
@@ -19,12 +20,13 @@ parser = argparse.ArgumentParser(
 parser.add_argument("-c", "--config", help="configuration file", type=str, default="etc/mqttutil.conf")
 parser.add_argument("-v", "--verbose", help="increase output verbosity", action="count", default=0)
 
-publish_options = parser.add_argument_group("mqtt")
+publish_options = parser.add_argument_group("publish")
 publish_options.add_argument("--mqtt-host", help="hostname of mqtt broker", default="localhost", type=str)
 publish_options.add_argument("--mqtt-port", help="port of mqtt broker", default=1883, type=int)
 publish_options.add_argument(
     "--json", help="publish json dict instead of primitive datatypes", action="store_true", default=False
 )
+publish_options.add_argument("--outpath", help="path for local storage", default=None, type=str)
 
 logger = logging.getLogger("mqttutil")
 
@@ -38,9 +40,10 @@ class Task:
         func: str,
         scheduling_interval: str,
         topic_prefix: str = f"{platform.node()}/mqttutil",
-        requires: List[str] = [],
+        requires: list[str] = [],
         qos: int = 0,
         test: bool = True,
+        outpath: str | None = None,
         **kwargs,
     ):
         super().__init__()
@@ -59,6 +62,14 @@ class Task:
         self.topic_prefix = topic_prefix
         self.topic_suffix = topic
         self.qos = qos
+
+        self.jsonl_path: str | None
+        if outpath:
+            self.jsonl_path = os.path.join(outpath, f"{self.topic}.jsonl")
+            jsonl_dir = os.path.dirname(self.jsonl_path)
+            os.makedirs(jsonl_dir, exist_ok=True)
+        else:
+            self.jsonl_path = None
 
         if test:
             try:
@@ -94,17 +105,26 @@ class Task:
     def _publish(self, pub_topic: str, result):
         # if json mode is set, publish json dict instead of primitive datatypes
         if self.json:
-            if isinstance(result, dict):
-                result_json = json.dumps(result)
-            elif isinstance(result, tuple) and hasattr(result, "_asdict"):
-                result_json = json.dumps(result._asdict())
-            elif isinstance(result, tuple) or isinstance(result, list):
-                result_json = json.dumps(dict(enumerate(result)))
-            else:
-                result_json = json.dumps({0: result})
+            result_dict: dict
 
+            if isinstance(result, dict):
+                result_dict = result
+            elif isinstance(result, tuple) and hasattr(result, "_asdict"):
+                result_dict = result._asdict()
+            elif isinstance(result, tuple) or isinstance(result, list):
+                result_dict = dict(enumerate(result))
+            else:
+                result_dict = {0: result}
+
+            result_json = json.dumps(result_dict)
             logger.info("publish %s %s", pub_topic, result_json)
             self.mqtt_c.publish(pub_topic, result_json, qos=self.qos)
+
+            if self.jsonl_path:
+                with open(self.jsonl_path, "a") as jsonl_file:
+                    jsonl_dict = {"_time": datetime.datetime.now().astimezone(), **result_dict}
+                    jsonl_file.write(json.dumps(jsonl_dict, default=str) + "\n")
+
             return
 
         # don't publish Nones
@@ -163,13 +183,13 @@ if __name__ == "__main__":
     config.read(args.config)
 
     # tasks
-    tasks: List[Task] = []
+    tasks: list[Task] = []
 
     # look for known sections
     for topic in config.sections():
         var = {k: literal_eval(v) for k, v in config.items(topic)}
         try:
-            tasks.append(Task(mqtt_c, args.json, topic, **var))
+            tasks.append(Task(mqtt_c, args.json, topic, outpath=args.outpath, **var))
         except Exception as e:
             logger.warning("Task '%s' cannot be created:", topic)
             logger.exception(e)
